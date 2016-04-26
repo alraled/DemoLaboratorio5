21df9f0b6850e66879d49f97f439f9558d13b1f8
alvaro.ramiro <alraled@hotmail.es>
2016-04-26 13:58:58 +0000

    Demo Laboratorio 5

---
+++ Demo Laboratorio 5/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="985c1e2e-c999-475a-b541-b2145237e0f2" description="SharePoint Add-in Feature" featureId="985c1e2e-c999-475a-b541-b2145237e0f2" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Demo Laboratorio 5 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6a9ced6b-8a35-4223-8343-85184307efcb" />
+    <projectItemReference itemId="ed864a9e-aa21-4112-ac51-6c23be2bc2b3" />
+    <projectItemReference itemId="a0223dc1-0e3d-4dd0-8da3-fb23ac104e01" />
+    <projectItemReference itemId="cf7a0790-d328-43e7-889d-5f5b30d265b1" />
+    <projectItemReference itemId="3e8ade9c-8ab8-4cb1-b805-d583366eed92" />
+    <projectItemReference itemId="ad59aa7b-8ef5-483d-a5a6-61a14f0325f4" />
+  </projectItems>
+</feature>
---
+++ Demo Laboratorio 5/Scripts/App.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+var Colegas = Window.Personas || {};  //ESTO ES UN NAMESPACE //
+
+Colegas.App = function () {
+
+    var personasList;
+    var personas;
+    var context;
+
+    var init = function () {
+        context = SP.ClientContext.get_current();
+        getAllPersonas();
+    };
+
+    var getAllPersonas = function () {
+        personasList = context.get_web().get_lists().getByTitle("Personas");
+        personas = personasList.getItems(new SP.CamlQuery());
+        context.load(personas);
+        context.ExecuteQueryAsync(OnPersonasSuccess, OnPersonasFail);
+    };
+
+    var onPersonasSuccess = function (sender, args) {
+    var html="<ul>"
+    var personasEnum = personas.getEnumerator();
+    while (personasEnum.moveNext()) {
+        var actual = personasEnum.get_current();
+        html+= "<li>"+actual.get_item("Nombre")+"" + actual.get_item("Edad") + "</li>"
+    }
+
+    html+= "</ul>"
+    $("#lista").html(html);
+
+    };
+
+    var onPersonasFail = function (sender, args) {
+        alert("Error" + args, get_message());
+    };
+
+    var crearPersona = function () {
+        var itemCreateInfo = new SP.ListItemCreationInformation(); //ESTO ES PARA CREAR ELEMENTOS EN UNA LISTA//
+        var actual = personasList.addItem(itemCreateInfo);
+        actual.set_item("Nombre",$("#input-nombre").val());
+        actual.set_item("Edad",$("#input-edad").val());
+        actual.update();
+        context.load(actual);
+        context.ExecuteQueryAsync(onCreateSuccess, onPersonasFail);
+    };
+    
+    var onCreateSuccess = function (sender, args){
+        getAllPersonas();
+    };
+
+    return {
+        getPersonas: getAllPersonas,
+        createPersona: crearPersona,
+        init: init
+    };
+
+};
+
+$(document).ready(function () {
+    $("#button-add").bind("click", Colegas.App.createPersona);
+    ExecuteOrDelayUntilScriptLoaded(Colegas.App.init, "sp.js");
+});
+
+
+
